847773ddc9a78b8059329abc1d852c1da85a92a0
IvanD1061 <ivan.duenas@wsu.edu>
2025-09-17 11:10:06 -0700

    Added the console controls
    This will get the input from the user. depending on which command they would like to use. It will then find the command

---
+++ src/Console/Console.c
@@ -0,0 +1,150 @@
+#include "Console.h"
+int getInput(TreeNode *cwd, char *command, char *path, char *name)
+{
+    char input[100];
+    pwd(cwd);
+    printf("$");
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = '\0';
+    char temp[100] = "";
+    char temp2[100] = "";
+    char temp3[100] = "";
+    char *token = NULL;
+    token = strtok(input, " ");
+    if (token == NULL)
+    {
+        return 0;
+    }
+    strcpy(temp, token);
+    strcpy(command, temp);
+
+    token = strtok(NULL, " ");
+
+    if (token == NULL)
+    {
+        name = NULL;
+        path = NULL;
+        return 1;
+    }
+    strcpy(temp2, token);
+    strcpy(path, temp2);
+
+    token = strtok(NULL, " ");
+    if (token == NULL)
+    {
+        name = NULL;
+        return 1;
+    }
+    strcpy(temp3, token);
+    strcpy(name, temp3);
+
+    token = strtok(NULL, " ");
+    if (token != NULL)
+    {
+        printf("Command '%s' not found.\n", temp);
+        return 0;
+    }
+    return 1;
+}
+
+int findCommand(TreeNode *cwd, char *command, char *path, char *name)
+{
+    if (strcmp(command, "quit") == 0)
+    {
+        return -1;
+    }
+    if (strcmp(command, "mkdir") == 0)
+    {
+        if ((strlen(name) == 0) && (strlen(path) == 0))
+        {
+            printf("'mkdir' missing operand\n");
+            return 0;
+        }
+        else if (strlen(name) == 0)
+        {
+            return mkdir(cwd, path, NULL);
+        }
+        return mkdir(cwd, name, path);
+    }
+    else if (strcmp(command, "rmdir") == 0)
+    {
+        if (strlen(name) != 0)
+        {
+            printf("'unknown operand %s\n'", name);
+            return 0;
+        }
+        return rmdir(cwd, path);
+    }
+    else if (strcmp(command, "creat") == 0)
+    {
+        if ((strlen(name) == 0) && (strlen(path) == 0))
+        {
+            printf("'creat' missing operand\n");
+            return 0;
+        }
+        else if (strlen(name) == 0)
+        {
+            return creat(cwd, path, NULL);
+        }
+        return creat(cwd, name, path);
+    }
+    else if (strcmp(command, "rm") == 0)
+    {
+        if (strlen(name) != 0)
+        {
+            printf("'unknown operand %s'\n", name);
+            return 0;
+        }
+        return rm(cwd, path);
+    }
+    else if (strcmp(command, "cd") == 0)
+    {
+        if (strlen(name) != 0)
+        {
+            printf("'unknown operand %s'\n", name);
+            return 0;
+        }
+        return -7;
+    }
+    else if (strcmp(command, "ls") == 0)
+    {
+        if (strlen(name) != 0)
+        {
+            printf("'unknown operand %s'\n", name);
+            return 0;
+        }
+        if (strlen(path) == 0)
+        {
+            ls(cwd, NULL);
+            return 1;
+        }
+        ls(cwd, path);
+        return 1;
+    }
+    else if (strcmp(command, "pwd") == 0)
+    {
+        if (strlen(path) != 0)
+        {
+            printf("'unknown operand %s'\n", path);
+            return 0;
+        }
+        pwd(cwd);
+        printf("\n");
+        return 1;
+    }
+    else if (strcmp(command, "save") == 0)
+    {
+
+        return -6;
+    }
+    else if (strcmp(command, "load") == 0)
+    {
+
+        return -8;
+    }
+    else
+    {
+        printf("Unknown command: %s\n", command);
+        return 0;
+    }
+}
