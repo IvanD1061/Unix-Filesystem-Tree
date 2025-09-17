ca6a52b60a282c0c1c09d4b9ef0947879c468e17
IvanD1061 <ivan.duenas@wsu.edu>
2025-09-17 11:11:28 -0700

    Added the new code to the Console.h

---
+++ src/Console/Console.h
@@ -0,0 +1,6 @@
+#include "../Serializer/Serializer.h"
+
+extern int getInput(TreeNode *cwd, char *command, char *path, char *name);
+
+
+extern int findCommand(TreeNode *cwd, char *command, char *path, char *name);
